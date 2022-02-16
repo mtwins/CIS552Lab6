3c8f318f0ca595d4daf102d3bf66b7929416c07a
Melissa Heredia <herediamelissa19@gmail.com>
2022-02-16 12:29:19 -0500

    Lab6 implementation

--- pong_game_start.py
+++ pong_game_start.py
@@ -2,6 +2,7 @@
 
 # import library
 from cis552 import *
+import random
 
 # Constants
 WINDOW_H = 400   # window height
@@ -24,8 +25,8 @@ lp_x = 0                     # x coordinate of left paddle
 lp_y = 0                     # y coordinate of left paddle
 rp_x = WINDOW_W-PADDLE_W     # x coordinate of right paddle
 rp_y = WINDOW_H-PADDLE_H     # y coordinate of right paddle
-b_x = WINDOW_W/2            # x coordinate of ball
-b_y = WINDOW_H/2            # y coordinate of ball
+b_x = random.randrange(0, WINDOW_W)  # x coordinate of ball
+b_y = random.randrange(0, WINDOW_H)   # y coordinate of ball
 vx = V                     # horizontal velocity of ball
 vy = V/4                     # vertical velocity of ball
 lp_up = False                # flag for left paddle up
@@ -33,11 +34,15 @@ lp_down = False             # flag for left paddle down
 rp_up = False                # flag for right paddle up
 rp_down = False              # flag for right paddle down
 new = False                  # flag for a new game
-
+current_color_index=0
+lscore=0
+rscore=0
+color = [(0.8, 0.8, 0.8), (1, 0.65, 0), (.01, .66, .62), (.5, 0, 0.5)]
 # Functions
 def draw_frame():
     global lp_x, lp_y, rp_x, rp_y
     global b_x, b_y, vx, vy, lp_up, lp_down, rp_up, rp_down, exit, new
+    global  current_color_index,lscore,rscore,color
 
     if new == True:
         lp_x = 0  # x coordinate of left paddle
@@ -53,14 +58,28 @@ def draw_frame():
         rp_up = False  # flag for right paddle up
         rp_down = False  # flag for right paddle down
         new = False
+        current_color_index=0
+        lscore=0
+        rscore=0
+
 
     set_clear_color(0.0, 0.0, 0.0)
     clear()
 
     disable_stroke()
-    set_fill_color(1, 0, 0)
-    draw_rectangle(lp_x, lp_y, PADDLE_W, PADDLE_H)
-    draw_rectangle(rp_x, rp_y, PADDLE_W, PADDLE_H)
+    #change padddle image
+    img = load_image("paddle.png")
+    draw_image(img,lp_x, lp_y)
+    draw_image(img,rp_x, rp_y)
+
+    # score board
+    enable_stroke()
+    set_stroke_width(1)
+    set_stroke_color(1, 1, 1)
+    sl = "Score left: " + str(lscore)
+    draw_text(sl, 100, 15)
+    sr = "Score right " + str(rscore)
+    draw_text(sr, 200, 15)
 
     # update y coordinate of left paddle
     lpy2 = lp_y
@@ -89,24 +108,41 @@ def draw_frame():
     # test horizontally
     if by2 > lp_y and by2 < lp_y+PADDLE_H and bx2 < BALL_R+PADDLE_W:       # test on left paddle
         vx = -vx
+        rotate_color()
     elif by2 > rp_y and by2 < rp_y+PADDLE_H and bx2 > WINDOW_W-BALL_R-PADDLE_W:       # test on right paddle
         vx = -vx
-    elif bx2 > WINDOW_W-BALL_R or bx2 < BALL_R:   # test on right or left side walls
+        rotate_color()
+    elif bx2 > WINDOW_W-BALL_R:   # test on right  side walls
         vx = -vx
+        rotate_color()
+        lscore+=1
+    elif  bx2 < BALL_R:  # test on left side walls
+        vx = -vx
+        rotate_color()
+        rscore += 1
 
     # test vertically
     elif bx2 > lp_x and bx2 < lp_x+PADDLE_W and (by2>lp_y-BALL_R or by2<lp_y+PADDLE_H+BALL_R):   # test on left paddle
         vy = -vy
+        rotate_color()
     elif bx2 > rp_x and bx2 < rp_x+ PADDLE_W and (by2>rp_y-BALL_R or by2<rp_y+PADDLE_H+BALL_R): # test on right paddle
         vy = -vy
+        rotate_color()
     elif by2 > WINDOW_H-BALL_R or by2 < BALL_R:   # test on lower and upper side walls
         vy = -vy
+        rotate_color()
 
     # update coordinates of ball and draw the ball
     b_x += vx*DT
     b_y += vy*DT
-    set_fill_color(0.8, 0.8, 0.8)
     draw_circle(b_x, b_y, BALL_R)
+
+
+def rotate_color():
+    global current_color_index, color
+    current_color_index = (current_color_index + 1) % 4
+    set_fill_color(color[current_color_index][0], color[current_color_index][1], color[current_color_index][2])
+
 
 def keydown(key):
     global lp_up, lp_down, rp_up, rp_down, new
